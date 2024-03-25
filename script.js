365f4daeeb4cf82035637e4cada36d04da5e5d6b
Ahmad Ali <spyali463@gmail.com>
2024-03-25 19:33:04 +0500

    sss

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-// let result = document.getElementById("inputText");
+let result = document.getElementById("inputText");
 
-// // =========================       Clear      =======================
-// const clr = () => {
-//     result.value = "";
-// }
-// // =========================       Delete      =======================
+// =========================       Clear      =======================
+const clr = () => {
+    result.value = "";
+}
+// =========================       Delete      =======================
 
-// const del = () => {
-//     result.value = result.value.slice(0 , -1)
-// }
+const del = () => {
+    result.value = result.value.slice(0 , -1)
+}
 
 
-// let Result = () => {
-//     try{
-//          result.value = eval(result.value)
-//     }
-//     catch(err){
-//         alert('Please Enter valid Input')
-//         return
-//     }
-// }
+let Result = () => {
+    try{
+         result.value = eval(result.value)
+    }
+    catch(err){
+        alert('Please Enter valid Input')
+        return
+    }
+}
 
 
 // =========================       numbers      =======================
-// let calculate = (number) => {
-//     result.value += number
-// }
-function appendToDisplay(value) {
-    document.getElementById('display').value += value;
-}
-
-function clearDisplay() {
-    document.getElementById('display').value = '';
-}
-
-function calculate() {
-    var expression = document.getElementById('display').value;
-    var result = eval(expression);
-    document.getElementById('display').value = result;
+let calculate = (number) => {
+    result.value += number
 }
